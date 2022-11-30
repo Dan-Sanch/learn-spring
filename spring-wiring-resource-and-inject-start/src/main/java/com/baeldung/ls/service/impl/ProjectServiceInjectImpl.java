c0e406bdd6825f6cc04853d9454a3d1ff77c45d8
Daniel Alberto Sanchez <Daniel-Alberto.Sanchez@endava.com>
2022-11-30 15:32:20 -0600

    Lessons 9 completed

---
+++ spring-wiring-resource-and-inject-start/src/main/java/com/baeldung/ls/service/impl/ProjectServiceInjectImpl.java
@@ -0,0 +1,35 @@
+package com.baeldung.ls.service.impl;
+
+import com.baeldung.ls.persistence.model.Project;
+import com.baeldung.ls.persistence.repository.IProjectRepository;
+import com.baeldung.ls.service.IProjectService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import java.util.Optional;
+
+@Service
+public class ProjectServiceInjectImpl implements IProjectService {
+    private static final Logger LOG = LoggerFactory.getLogger(ProjectServiceImpl.class);
+    private IProjectRepository projectRepository;
+
+    @Inject
+    @Named("projectRepositoryImpl") // NOTE: Unneeded, but can be used to avoid ambiguity
+    public ProjectServiceInjectImpl(IProjectRepository projectRepository) {
+        this.projectRepository = projectRepository;
+        LOG.info("wired projectRepository instance: {}", projectRepository);
+    }
+
+    @Override
+    public Optional<Project> findById(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Project save(Project project) {
+        return null;
+    }
+}
