c063c1030c445b68a2f908623cde65382d46454a
Daniel Alberto Sanchez <Daniel-Alberto.Sanchez@endava.com>
2023-01-10 13:23:45 -0600

    Module 7-2 Implemented "Create record" endpoint

--- m7-full-create-operation-with-thymeleaf-start/src/main/java/com/baeldung/ls/web/controller/ProjectController.java
+++ m7-full-create-operation-with-thymeleaf-start/src/main/java/com/baeldung/ls/web/controller/ProjectController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.baeldung.ls.persistence.model.Project;
@@ -35,6 +36,19 @@ public class ProjectController {
         projects.forEach(p -> projectDtos.add(convertToDto(p)));
         model.addAttribute("projects", projectDtos);
         return "projects";
+    }
+
+    @GetMapping("/new")
+    public String newProject(Model model) {
+        model.addAttribute("project", new ProjectDto());
+        return "new-project";
+    }
+
+    @PostMapping
+    public String addProject(ProjectDto project) {
+        projectService.save(convertToEntity(project));
+
+        return "redirect:/projects";
     }
 
     //
